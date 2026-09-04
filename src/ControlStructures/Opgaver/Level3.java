278947b43faf02fe9901c889c15d3cff2deeaf8c
EmilMarinovic <emilmarinovic@outlook.dk>
2026-09-04 20:18:40 +0200

    cs opgave level 3

--- src/ControlStructures/Opgaver/Level3.java
+++ src/ControlStructures/Opgaver/Level3.java
@@ -1,5 +1,7 @@
 package ControlStructures.Opgaver;
 
+//import java.util.Scanner;
+
 public class Level3 {
 // Concept: https://github.com/Dat1Cphbusiness/WorkingWithControlStructures/blob/main/concepts/level3.md
     // Assignment: https://github.com/Dat1Cphbusiness/WorkingWithControlStructures/blob/main/assignments/level3.md
@@ -131,6 +133,377 @@ public class Level3 {
             default:
                 System.out.println("Type a traffic light color (red, yellow or green)");
         }
+         */
+
+        //Opgave 3.7
+        /*
+        String month = "March";
+        switch (month) {
+            case "December":
+            case "January":
+            case "February":
+                System.out.println("Winter");
+                break;
+            case "March":
+            case "April":
+            case "May":
+                System.out.println("Spring");
+                break;
+            case "June":
+            case "July":
+            case "August":
+                System.out.println("Summer");
+                break;
+            case "September":
+            case "October":
+            case "November":
+                System.out.println("Fall");
+                break;
+        }
+         */
+
+        //Opgave 3.8
+        /*
+        String operator = "divide";
+        int a = 10;
+        int b = 5;
+        int result;
+        switch (operator) {
+            case "add":
+                System.out.println("10 + 5 = " + (a + b));
+                break;
+            case "subtract":
+                System.out.println("10 - 5 = " + (a - b));
+                break;
+            case "multiply":
+                System.out.println("10 * 5 = " + (a *b));
+                break;
+            case "divide":
+                System.out.println("10 / 5 = " + (a / b));
+                break;
+        }
+         */
+
+        //Opgave 3.9
+        /*
+        int day = 6;
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+         */
+
+
+        //Opgave 3.10
+        /*
+        char ch = 'e';
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+         */
+
+        //Opgave 3.11
+        /*
+        char grade = 'B';
+        switch (grade) {
+            case 'A':
+            case 'B':
+                System.out.println("Good");
+                break;
+            case 'C':
+            case 'D':
+                System.out.println("Average");
+                break;
+            case 'E':
+            case 'F':
+                System.out.println("Poor");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+         */
+
+        //Opgave 3.12
+        /*
+        int num = 5;
+        switch (num) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Low");
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                System.out.println("Medium");
+                break;
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("Out of range");
+                break;
+        }
+         */
+
+        //Opgave 3.13
+        /*
+        int month = 7;
+        int days;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                days = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                days = 30;
+                break;
+            case 2:
+                days = 28;
+                break;
+            default:
+                days = 0;
+                System.out.println("Invalid month");
+        }
+        System.out.println("Days: " + days);
+         */
+
+        //Opgave 3.14
+        /*
+        1. if-else er bedst
+        2. switch er bedst
+        3. switch er bedst
+        4. if-else er bedst
+         */
+
+        //Opgave 3.15
+        /*
+        int choice = 2;
+        switch (choice) {
+            case 1:
+                System.out.println("Option 1");
+                break;
+            case 2:
+                System.out.println("Option 2");
+                break;
+            case 3:
+                System.out.println("Option 3");
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+         */
+
+        //Opgave 3.16
+        /*
+        Det kan jeg ikke fordi det er givet ranges. Jeg ville godt kunne, men det ville betyde at jeg skal lave en sindsygt mange cases, hvilket ikke er effektivt
+         */
+
+        //Opgave 3.17
+        /*
+        int age;
+        int price;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Indtast din alder: ");
+        age = input.nextInt();
+        if (age >= 1 && age < 12) {
+            price = 50;
+        }
+        else if (age >= 13 && age < 17) {
+            price = 75;
+        }
+        else if (age >= 18 && age < 64) {
+            price = 100;
+        }
+        else {
+            price = 60;
+        }
+        System.out.println("Prisen bliver: " + price);
+         */
+
+        //Opgave 3.18
+        /*
+        int month = 5;
+        String quarter;
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                quarter = "Q1";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                quarter = "Q2";
+                break;
+            case 7:
+            case 8:
+            case 9:
+                quarter = "Q3";
+                break;
+            case 10:
+            case 11:
+            case 12:
+                quarter = "Q4";
+                break;
+            default:
+                quarter = "Invalid";
+                System.out.println("Invalid month");
+                break;
+        }
+
+        System.out.println("Quarter: " + quarter);
+         */
+
+        //Opgave 3.19
+        /*
+        char grade = 'B';
+        int points;
+        switch (grade) {
+            case 'A':
+                points = 12;
+                break;
+            case 'B':
+                points = 10;
+                break;
+            case 'C':
+                points = 7;
+                break;
+            case 'D':
+                points = 4;
+                break;
+            case 'F':
+                points = 0;
+                break;
+            default:
+                points = -1;
+                System.out.println("Invalid grade");
+        }
+        if (points >= 0) {
+            System.out.println("Points: " + points);
+        }
+         */
+
+        //Opgave 3.20
+        /*
+        String category = "fruit";
+        String item = "apple";
+        String description;
+        switch (category) {
+            case "fruit":
+                if (item.equals("apple")) {
+                    description = "Red apple";
+                }
+                else if (item.equals("banana")) {
+                    description = "Yellow banana";
+                }
+                else {
+                    description = "Invalid";
+                    System.out.println("Unknown fruit");
+                }
+            break;
+            case "vegetable":
+                if (item.equals("carrot")) {
+                    description = "Orange carrot";
+                }
+                else {
+                    description = "Invalid";
+                    System.out.println("Unknown vegetable");
+                }
+                break;
+            default:
+                description = "Unknown";
+                System.out.println("Unknown category");
+        }
+        System.out.println("Description: " + description);
+         */
+
+        //Opgave 3.21
+        /*
+        int a = 15;
+        int b = 3;
+        String operation = "divide";
+        double result;
+        boolean validOperation = true;
+        switch (operation) {
+            case "add":
+                result = a + b;
+                break;
+            case "subtract":
+                result = a - b;
+                break;
+            case "multiply":
+                result = a * b;
+            case "divide":
+                if (b !=0) {
+                    result = a / b;
+                }
+                else {
+                    System.out.println("You can't divide by 0");
+                    validOperation = false;
+                    result = 0;
+                }
+                break;
+            default:
+                System.out.println("Please use add, subtract, multiply or division");
+                validOperation = false;
+                result = 0;
+        }
+        if (validOperation) {
+            System.out.println(result);
+        }
+         */
+
+        //Opgave 3.22
+        /*
+        int day = 3;
+        int workHours;
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                workHours = 8;
+                break;
+            case 5:
+                workHours = 6;
+                break;
+            case 6:
+            case 7:
+                workHours = 0;
+                break;
+            default:
+                workHours = 0;
+                System.out.println("Invalid day");
+        }
+        System.out.println("Work hours: " + workHours);
          */
     }
 }
