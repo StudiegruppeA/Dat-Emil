27ac8d28ccf2e5ae479497d149e3a7e9cb4ead4f
EmilMarinovic <emilmarinovic@outlook.dk>
2026-09-11 13:27:16 +0200

    level1m

--- src/Mehods/Opgaver/Level1.java
+++ src/Mehods/Opgaver/Level1.java
@@ -2,7 +2,7 @@ package Mehods.Opgaver;
 
 public class Level1 {
 
-    // Opgave 1.1
+        // Opgave 1.1
     /*
     void sayHello() {
         System.out.println("Hello!");
@@ -13,7 +13,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.2
+        // Opgave 1.2
     /*
     void sayHello() {
         System.out.println("Hello!");
@@ -26,7 +26,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.3
+        // Opgave 1.3
     /*
     void printWelcome() {
         System.out.println("Welcome to the program!");
@@ -38,22 +38,22 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.4
-    // Den printer:
+        // Opgave 1.4
+        // Den printer:
     /*
     Before
     Hi there!
     After
      */
 
-    // Opgave 1.5
-    // Den printer:
+        // Opgave 1.5
+        // Den printer:
     /*
     Start
     End
      */
 
-     // Opgave 1.6
+        // Opgave 1.6
     /*
     void first() {
         System.out.println("First");
@@ -69,7 +69,7 @@ public class Level1 {
     }
     */
 
-    // Opgave 1.7
+        // Opgave 1.7
     /*
     String appName = "MyApp";
 
@@ -82,7 +82,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.8
+        // Opgave 1.8
     /*
     String title = "Game";
     int version = 2;
@@ -103,7 +103,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.9
+        // Opgave 1.9
     /*
     void printLine() {
         System.out.println("----------");
@@ -116,7 +116,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.10
+        // Opgave 1.10
     /*
     void printEmptyLine() {
         System.out.println();
@@ -129,7 +129,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.11
+        // Opgave 1.11
     /*
     void printBox() {
         System.out.println("+-----+");
@@ -142,7 +142,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.12
+        // Opgave 1.12
     /*
     void printPattern() {
         System.out.println("* * *");
@@ -155,7 +155,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.13
+        // Opgave 1.13
     /*
     void printMenu() {
         System.out.println("1. Start");
@@ -168,7 +168,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.14
+        // Opgave 1.14
     /*
     void printGreeting() {
         System.out.println("Velkommen til skolen.");
@@ -181,7 +181,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.15
+        // Opgave 1.15
     /*
     void printSmile() {
         System.out.println(":)");
@@ -196,7 +196,7 @@ public class Level1 {
     }
      */
 
-    // Opgave 1.16
+        // Opgave 1.16
     /*
     void printSeperator() {
         System.out.println("====================");
@@ -210,6 +210,233 @@ public class Level1 {
      */
 
     // Opgave 1.17
+    /*
+    void printHello() {
+        System.out.println("Hello");
+    }
+
+    void printGoodbye() {
+        printHello();
+    }
+
+    void main() {
+        printGoodbye();
+    }
+     */
+
+    // Opgave 1.18
+    /*
+    void first() {
+        System.out.println("First");
+    }
+
+    void second() {
+        System.out.println("Second");
+    }
+
+    void third() {
+        System.out.println("Third");
+    }
+
+    void main() {
+        third();
+        second();
+        first();
+    }
+     */
+
+    // Opgave 1.19
+    /*
+    void wakeUp() {
+        System.out.println("Wake up!");
+    }
+
+    void eatBreakfast() {
+        System.out.println("Eating breakfast");
+    }
+
+    void brushTeeth() {
+        System.out.println("Brushing teeth");
+    }
+
+    void main() {
+        wakeUp();
+        brushTeeth();
+        eatBreakfast();
+    }
+     */
+
+    // Opgave 1.20
+    /*
+    void monday() {
+        System.out.println("Monday");
+    }
+
+    void tuesday() {
+        System.out.println("Tuesday");
+    }
+
+    void wednesday() {
+        System.out.println("Wednesday");
+    }
+
+    void thursday() {
+        System.out.println("Thursday");
+    }
+
+    void friday() {
+        System.out.println("Friday");
+    }
+
+    void saturday() {
+        System.out.println("Saturday");
+    }
+
+    void sunday() {
+        System.out.println("Sunday");
+    }
+
+    void main() {
+        saturday();
+        sunday();
+    }
+     */
+
+    // Opgave 1.21
+    /*
+    void red() {
+        System.out.println("STOP");
+    }
+
+    void yellow() {
+        System.out.println("SLOW DOWN");
+    }
+
+    void green() {
+        System.out.println("GO");
+    }
+
+    void main() {
+        red();
+        green();
+        yellow();
+        red();
+    }
+     */
+
+    // Opgave 1.22
+    /*
+    void printStars() {
+        System.out.println("***");
+    }
+
+    void printDashed() {
+        System.out.println("---");
+    }
+
+    void main() {
+        printStars();
+        printDashed();
+        printStars();
+        printDashed();
+        printStars();
+    }
+     */
+
+    // Opgave 1.23
+    /*
+    void printBeep() {
+        System.out.println("BEEP!");
+    }
+
+    void main() {
+        for (int i = 1; i <= 10; i++) {
+            printBeep();
+        }
+    }
+     */
+
+    // Opgave 1.24
+    /*
+    void printHello() {
+        System.out.println("Hello");
+    }
+
+    void printGoodbye() {
+        System.out.println("Goodbye");
+    }
+
+    void main() {
+        printHello();
+        printGoodbye();
+        printHello();
+        printGoodbye();
+        printHello();
+    }
+     */
+
+    // Opgave 1.25
+    /*
+    void printHeader() {
+        System.out.println("=== REPORT ===");
+    }
+
+    void printBody() {
+        System.out.println("Data goes here");
+    }
+
+    void printFooter(){
+        System.out.println("=== END ===");
+    }
+
+    void main() {
+        printHeader();
+        printBody();
+        printFooter();
+    }
+     */
+
+    // Opgave 1.26
+    /*
+    void printTitle() {
+        System.out.println("=== DATAMATIKER ===");
+    }
+
+    void printContent() {
+        System.out.println("Du går på EK Firskovvej");
+        System.out.println("Det er i Lyngby");
+    }
+
+    void printPageNumber() {
+        System.out.println("Page 1");
+    }
+
+    void main() {
+        printTitle();
+        System.out.println();
+        printContent();
+        System.out.println();
+        printPageNumber();
+    }
+     */
+
+    // Opgave 1.27
+    /*
+    void printDivider() {
+        System.out.println("--------------------");
+    }
+
+    void main() {
+        System.out.println("Hej");
+        printDivider();
+        System.out.println("med");
+        printDivider();
+        System.out.println("dig");
+    }
+     */
+
+    // Opgave 1.28
+
 
 
 
