794c6b5088d13ef40937b7a1ea0e461e91bec37d
EmilMarinovic <emilmarinovic@outlook.dk>
2026-09-13 09:23:29 +0200

    level2m

--- src/Mehods/Methods.java
+++ src/Mehods/Methods.java
@@ -1,4 +1,21 @@
 package Mehods;
 
 public class Methods {
+
+
+    void printIfPositive(int number) {
+        if (number > 0) {
+            System.out.println(number);
+        }
+    }
+
+    void main() {
+        printIfPositive(-5);
+        printIfPositive(0);
+        printIfPositive(10);
+        printIfPositive(20);
+    }
+
+
+
 }
---
+++ src/Mehods/Opgaver/Level2.java
@@ -0,0 +1,330 @@
+package Mehods.Opgaver;
+
+public class Level2 {
+
+    // Opgave 2.1
+    /*
+    void printname(String name) {
+        System.out.println("Name: " + name);
+    }
+
+    void main() {
+        printname("Emil");
+    }
+     */
+
+    // Opgave 2.2
+    /*
+    void printname(String name) {
+        System.out.println("Name: " + name);
+    }
+
+    void main() {
+        printname("Krsta");
+        printname("Smilja");
+        printname("Jannick");
+    }
+     */
+
+    // Opgave 2.3
+    /*
+    void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    void main() {
+        greet("Emil");
+        greet("Jens");
+        greet("Marko");
+    }
+     */
+
+    // Opgave 2.4
+    /*
+    void greet(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    void main() {
+        String myName = "Emil";
+        greet(myName);
+    }
+     */
+
+    // Opgave 2.5
+    /*
+    void printMessage(String message) {
+        System.out.println("*** " + message + " ***");
+    }
+
+    void main() {
+        printMessage("BREAKING");
+        printMessage("DONUT");
+    }
+     */
+
+    // Opgave 2.6
+    /*
+    Outputtet bliver:
+    >>> Start <<<
+    >>> Middle <<<
+    >>> End <<<
+     */
+
+    // Opgave 2.7
+    /*
+    void shout(String text) {
+        System.out.println(text.toUpperCase());
+    }
+
+    void main() {
+        shout("det kan altså ikke passe");
+    }
+     */
+
+    // Opgave 2.8
+    /*
+    void whisper(String text) {
+        System.out.println(text.toLowerCase());
+    }
+
+    void main() {
+        whisper("DET KAN MÅSKE PASSE");
+    }
+     */
+
+    // Opgave 2.9
+    /*
+    void printNumber(int number) {
+        System.out.println("Number: " + number);
+    }
+
+    void main() {
+        printNumber(10);
+        printNumber(42);
+        printNumber(100);
+    }
+     */
+
+    // Opgave 2.10
+    /*
+    void printPrice(double price) {
+        System.out.println("Price: " + price + " kr");
+    }
+
+    void main() {
+        printPrice(29.96);
+        printPrice(149.00);
+    }
+     */
+
+    // Opgave 2.11
+    /*
+    void printStatus(boolean active) {
+        System.out.println("Active: " + active);
+    }
+
+    void main() {
+        printStatus(true);
+        printStatus(false);
+    }
+     */
+
+    // Opgave 2.12
+    /*
+    void printGrade(char grade) {
+        System.out.println("Grade: " + grade);
+    }
+
+    void main() {
+        printGrade('A');
+        printGrade('B');
+        printGrade('C');
+    }
+     */
+
+    // Opgave 2.13
+    /*
+    void printScore(int points) {
+        System.out.println("Score: " + points);
+    }
+
+    void main() {
+        int score = 85;
+        printScore(score);
+    }
+     */
+
+    // Opgave 2.14
+    /*
+    void printNumber(int number) {
+        System.out.println("Number: " + number);
+    }
+
+    void main() {
+        printNumber(5 + 3);
+        printNumber(10 * 2);
+        printNumber(100 / 4);
+    }
+     */
+
+    // Opgave 2.15
+    /*
+    void printTemperature (double temp) {
+        System.out.println("Temperature: " + temp + " C");
+    }
+
+    void main() {
+        printTemperature(22);
+    }
+     */
+
+    // Opgave 2.16
+    /*
+    void printPercentage (int percent) {
+        System.out.println(percent + "%");
+    }
+
+    void main() {
+        printPercentage(20);
+    }
+     */
+
+    // Opgave 2.17
+    /*
+    void printFullName (String firstName, String lastName) {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    void main() {
+        printFullName("Ida", "Jensen");
+        printFullName("Emil", "Marinovic");
+    }
+     */
+
+    // Opgave 2.18
+    /*
+    void printPerson(String name, int age) {
+        System.out.println(name + " is " + age + " years old");
+    }
+
+    void main() {
+        printPerson("Emil", 20);
+    }
+     */
+
+    // Opgave 2.19
+    /*
+    void printProduct(String name, double price, int quantity) {
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println();
+    }
+
+    void main() {
+        printProduct("Iphone", 9995.50, 2);
+        printProduct("Egelykke koldskål", 10.0, 1);
+    }
+     */
+
+    // Opgave 2.20
+    /*
+    Den printer:
+    Score = 100
+    Lives = 3
+     */
+
+    // Opgave 2.21
+    /*
+    void printLabeled(String label, String value) {
+        System.out.println(label + ": " + value);
+    }
+
+    void main() {
+        printLabeled("School", "Datamatiker");
+    }
+     */
+
+    // Opgave 2.22
+    /*
+    void printRectangle(int width, int height) {
+        System.out.println("Rectangle: " + width + " x " + height);
+    }
+
+    void main() {
+        printRectangle(20, 10);
+    }
+     */
+
+    // Opgave 2.23
+    /*
+    void printCoordinates(int x, int y) {
+        System.out.println("(" + x + ", " + y + ")");
+    }
+
+    void main() {
+        printCoordinates(4, 9);
+    }
+     */
+
+    // Opgave 2.24
+    /*
+    void printRange(int min, int max) {
+        System.out.println("Range: " + min + " to " + max);
+    }
+
+    void main() {
+        printRange(10, 30);
+    }
+     */
+
+    // Opgave 2.25
+    /*
+    void printIfPositive(int number) {
+        if (number > 0) {
+            System.out.println(number);
+        }
+    }
+
+    void main() {
+        printIfPositive(-2);
+    }
+     */
+
+    // Opgave 2.26
+    /*
+    void printEvenOdd(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+        }
+        else {
+            System.out.println(number + " is odd");
+        }
+    }
+
+    void main() {
+        printEvenOdd(21);
+    }
+     */
+
+    // Opgave 2.27
+    /*
+    void printAgeCategory(int age) {
+        if (age < 13) {
+            System.out.println("Child");
+        }
+        else if (age <= 19 && age >= 13) {
+            System.out.println("Teenager");
+        }
+        else {
+            System.out.println("Adult");
+        }
+    }
+
+    void main() {
+        printAgeCategory(99);
+    }
+     */
+
+}
